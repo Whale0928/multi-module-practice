5e49a5f13fecb0c7c2b926ba58511541212eb800
KIM HYUN GI <rlagusrl928@gmail.com>
2024-01-18 21:16:31 +0900

    create baseball test case

--- module-baseball/src/main/java/app/multimodule/modulebaseball/BaseBallGame.java
+++ module-baseball/src/main/java/app/multimodule/modulebaseball/BaseBallGame.java
@@ -1,4 +1,23 @@
 package app.multimodule.modulebaseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class BaseBallGame {
+
+	public Integer getRandom() {
+		return new Random().nextInt(9) + 1;
+	}
+
+	public List<Integer> getStrikeZones() {
+		List<Integer> strikeZones = new ArrayList<>();
+		while (strikeZones.size() < 3) {
+			Integer random = getRandom();
+			if (!strikeZones.contains(random)) {
+				strikeZones.add(random);
+			}
+		}
+		return strikeZones;
+	}
 }
--- module-baseball/src/test/java/app/multimodule/modulebaseball/BaseBallTest.java
+++ module-baseball/src/test/java/app/multimodule/modulebaseball/BaseBallTest.java
@@ -1,36 +1,55 @@
 package app.multimodule.modulebaseball;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @DisplayName("BaseBallTest")
 public class BaseBallTest {
 
-    @Test
-    @DisplayName("nothing test")
-    void nothing() {
-        System.out.println("BaseBallTest");
-    }
+	@Test
+	@DisplayName("nothing test")
+	void nothing() {
+		BaseBallGame baseBallGame = new BaseBallGame();
+	}
 
 
-    @Test
-    @DisplayName("stream test")
-    void stream() {
+	@Test
+	@DisplayName("1~9 사이의 랜덤한 값을 만들수 있다.")
+	void can_make_random() {
+		Integer randomValue = new BaseBallGame().getRandom();
+		Assertions.assertTrue(randomValue >= 1 && randomValue <= 9);
+	}
 
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+	@Test
+	@DisplayName("1~9 사이의 중복되지 않은 랜덤한 값을 3번 만들수 있다.")
+	void can_make_random_three_times() {
+		BaseBallGame game = new BaseBallGame();
+		List<Integer> randoms = game.getStrikeZones();
 
-        // Using parallel Stream API to process data
-        List<Integer> squaredNumbers = numbers.parallelStream()
-                .map(n -> {
-                    System.out.println("Processing " + n + " on thread " + Thread.currentThread().getName());
-                    return n * n;
-                })
-                .collect(Collectors.toList());
+		System.out.println(randoms);
+		Assertions.assertEquals(3, randoms.size());
+		Assertions.assertNotEquals(randoms.get(0), randoms.get(1));
+		Assertions.assertNotEquals(randoms.get(0), randoms.get(2));
+		Assertions.assertNotEquals(randoms.get(1), randoms.get(2));
+	}
 
-        System.out.println("Squared numbers: " + squaredNumbers);
-    }
+	@Test
+	@DisplayName("수를 입력받아 스트라이크를 판단할 수 있다.")
+	void is_strike() {
+		List<Integer> strikeZones = new LinkedList<>() {
+			{
+				add(1);
+				add(2);
+				add(3);
+			}
+		};
+
+		Integer input = 1;
+
+		Assertions.assertTrue(strikeZones.contains(input));
+	}
 }
