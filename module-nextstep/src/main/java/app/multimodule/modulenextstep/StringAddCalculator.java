5d5555dcba3b7f877beb7f5ec65adafcef62c60a
deadWhale <rlagusrl928@gmail.com>
2024-01-25 14:51:38 +0900

    update module nextstep : >> StringAddCalculator(문자열 계산기)

--- module-nextstep/src/main/java/app/multimodule/modulenextstep/StringAddCalculator.java
+++ module-nextstep/src/main/java/app/multimodule/modulenextstep/StringAddCalculator.java
@@ -1,8 +1,62 @@
 package app.multimodule.modulenextstep;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class StringAddCalculator {
-	public static int splitAndSum(Object o) {
-		return 0;
+
+	public static final String DEFAULT_DELIMITER = ",|:";
+
+	public static int splitAndSum(String input) {
+		if (Objects.isNull(input) || input.isBlank())
+			return 0;
+
+		String delimiter = getDelimiter(input);
+		int customIndexAt = getCustomIndexAt(input);
+
+		String[] numbers = getIntArrays(input, delimiter, customIndexAt);
+
+		if (numbers.length < 1)
+			return 0;
+
+		return Arrays.stream(numbers)
+			.mapToInt(s -> {
+				int value = Integer.parseInt(s);
+				if (value < 0)
+					throw new RuntimeException("음수는 입력할 수 없습니다.");
+				return value;
+			})
+			.sum();
 	}
-	
+
+	public static String[] getIntArrays(String input, String delimiter, int customIndexAt) {
+		if (customIndexAt != -1) {
+			input = input.substring(customIndexAt + 1);
+		}
+		return input.split(delimiter);
+	}
+
+	public static int getCustomIndexAt(String input) {
+
+		return input.indexOf("\n");
+	}
+
+	public static String getDelimiter(String input) {
+
+		int startAt = -1;
+		int endAt = -1;
+
+		if (input.startsWith("//")) {
+			startAt = 2;
+			endAt = input.indexOf("\n");
+
+			if (endAt == -1)
+				throw new RuntimeException("구분자가 잘못되었습니다.");
+			String substring = input.substring(startAt, endAt);
+			return substring + "|" + DEFAULT_DELIMITER;
+		}
+
+		return DEFAULT_DELIMITER;
+	}
+
 }
--- module-nextstep/src/test/java/app/multimodule/modulenextstep/StringAddCalculatorTest.java
+++ module-nextstep/src/test/java/app/multimodule/modulenextstep/StringAddCalculatorTest.java
@@ -2,6 +2,11 @@ package app.multimodule.modulenextstep;
 
 import org.junit.jupiter.api.Test;
 
+import static app.multimodule.modulenextstep.StringAddCalculator.DEFAULT_DELIMITER;
+import static app.multimodule.modulenextstep.StringAddCalculator.getCustomIndexAt;
+import static app.multimodule.modulenextstep.StringAddCalculator.getDelimiter;
+import static app.multimodule.modulenextstep.StringAddCalculator.getIntArrays;
+import static app.multimodule.modulenextstep.StringAddCalculator.splitAndSum;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -9,40 +14,74 @@ public class StringAddCalculatorTest {
 
 	@Test
 	public void splitAndSum_null_또는_빈문자() {
-		int result = StringAddCalculator.splitAndSum(null);
+		int result = splitAndSum(null);
 		assertThat(result).isEqualTo(0);
 
-		result = StringAddCalculator.splitAndSum("");
+		result = splitAndSum("");
 		assertThat(result).isEqualTo(0);
 	}
 
 	@Test
 	public void splitAndSum_숫자하나() throws Exception {
-		int result = StringAddCalculator.splitAndSum("1");
+		int result = splitAndSum("1");
 		assertThat(result).isEqualTo(1);
 	}
 
 	@Test
 	public void splitAndSum_쉼표구분자() throws Exception {
-		int result = StringAddCalculator.splitAndSum("1,2");
+		int result = splitAndSum("1,2");
 		assertThat(result).isEqualTo(3);
 	}
 
+
 	@Test
 	public void splitAndSum_쉼표_또는_콜론_구분자() throws Exception {
-		int result = StringAddCalculator.splitAndSum("1,2:3");
+		int result = splitAndSum("1,2:3");
 		assertThat(result).isEqualTo(6);
+	}
+
+
+	@Test
+	public void getCustomIndexAt_쉼표_또는_콜론_구분자() throws Exception {
+		int result1 = getCustomIndexAt("1,2:3");
+		int result2 = getCustomIndexAt("//;\n1;2;3");
+
+		assertThat(result1).isEqualTo(-1);
+		assertThat(result2).isEqualTo(3);
+	}
+
+	@Test
+	public void getDelimiter_구분자() throws Exception {
+		String delimiter1 = getDelimiter("//;\n1;2;3");
+		String delimiter2 = getDelimiter("//;;;;\n1;2;3");
+
+		assertThat(delimiter1).isEqualTo(";" + "|" + DEFAULT_DELIMITER);
+		assertThat(delimiter2).isEqualTo(";;;;" + "|" + DEFAULT_DELIMITER);
+	}
+
+	@Test
+	public void getIntArrays_test() throws Exception {
+		String input = "//;\n1;2;3";
+		String delimiter = ";|" + DEFAULT_DELIMITER;
+		int customIndexAt = getCustomIndexAt(input);
+
+		String[] intArrays = getIntArrays(input, delimiter, customIndexAt);
+
+		assertThat(intArrays.length).isEqualTo(3);
+		assertThat(intArrays[0]).isEqualTo("1");
+		assertThat(intArrays[1]).isEqualTo("2");
+		assertThat(intArrays[2]).isEqualTo("3");
 	}
 
 	@Test
 	public void splitAndSum_custom_구분자() throws Exception {
-		int result = StringAddCalculator.splitAndSum("//;\n1;2;3");
+		int result = splitAndSum("//;\n1;2;3");
 		assertThat(result).isEqualTo(6);
 	}
 
 	@Test
 	public void splitAndSum_negative() throws Exception {
-		assertThatThrownBy(() -> StringAddCalculator.splitAndSum("-1,2,3"))
+		assertThatThrownBy(() -> splitAndSum("-1,2,3"))
 			.isInstanceOf(RuntimeException.class);
 	}
 }
